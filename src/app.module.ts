b2bfcdcfede962d087adcc00f85273b251880967
Firasath <mohammed.hussain@innovasolutions.com>
2026-09-21 14:51:18 +0530

    Add cloud build config changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,11 +8,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'postgres-db',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'task_tracker',
+      host: process.env.DB_HOST ?? 'localhost',
+      port: Number(process.env.DB_PORT ?? 5432),
+      username: process.env.DB_USERNAME ?? 'postgres',
+      password: process.env.DB_PASSWORD ?? 'postgres',
+      database: process.env.DB_NAME ?? 'task_tracker',
       autoLoadEntities: true,
       synchronize: true,
     }),
